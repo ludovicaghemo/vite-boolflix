4750774c5d9c4c671b193654bdc18b91603cb440
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-11-29 17:56:08 +0100

    add: header component + search handling + api call

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
 import { store } from "./store.js";
+import axios from "axios";
 
 export default {
   data() {
@@ -10,14 +12,28 @@ export default {
   created() {
   },
   components: {
-  },
+    AppHeader
+},
   methods: {
+    handleBtn() {
+      axios
+      .get(this.store.apiUrl + this.store.movieSearch, {
+        params: {
+          query: this.store.searchText,
+          api_key: this.store.apiKey
+        }
+      })
+      .then((resp) => {
+        //console.log(resp);
+        this.store.moviesList = resp.data.results
+      })
+    }
   },
 };
 </script>
 
 <template>
-  
+  <AppHeader @get-search="handleBtn"/>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,38 @@
+<script>
+import { store } from '../store';
+export default {
+    emits: ["get-search"],
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <div class="container d-flex h-100 justify-content-between align-items-center">
+            <h1>BOOLFLIX</h1>
+            <div class="search-section">
+                <input type="text" name="" id="" class="me-2" placeholder="inizia la ricerca" v-model.trim="store.searchText">
+                <button @click="$emit('get-search')">CERCA</button>
+            </div>
+
+        </div>
+
+    </header>
+</template>
+
+<style scoped lang="scss">
+header {
+    height: 100px;
+}
+.container {
+    width: 90%;
+    margin: 0 auto;
+    h1 {
+        color: red;
+    }
+}
+</style>
