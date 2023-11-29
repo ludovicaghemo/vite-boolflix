352f2eef176efa471ea1324c316225e662ba21c5
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-11-29 14:17:08 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import { store } from "./store.js";
+
+export default {
+  data() {
+    return {
+      store: store
+    };
+  },
+  created() {
+  },
+  components: {
+  },
+  methods: {
+  },
+};
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+</style>
