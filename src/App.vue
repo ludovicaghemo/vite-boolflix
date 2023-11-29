5aca81af9046d9159a5c7998fc50b83c0c7f932d
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-11-29 15:01:19 +0100

    add: milestone 0 - global state structure

--- src/App.vue
+++ src/App.vue
@@ -22,4 +22,5 @@ export default {
 
 <style lang="scss">
 @use "./style/general.scss";
+@import "@fortawesome/fontawesome-free/css/all.css";
 </style>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,11 @@
 import { reactive, } from "vue";
 
 export const store = reactive({
-  
+    moviesList: [],
+    seriesList: [],
+    apiUrl: 'https://api.themoviedb.org/3/search',
+    apiKey: '6d528e53b247ff47c3c0b248b11a367e',
+    movieSearch: '/movie',
+    tvSeriesSearch: '/tv',
+    searchText: ''
 });
